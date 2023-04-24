e31008deb82fd3d34dd962618d2fc1b3d65aa2cb
Gaurav Borade <gauravborade618@gmail.com>
2023-04-24 20:15:34 +0530

    added controller and router for home

--- AuraVibe/index.js
+++ AuraVibe/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//* using express router from index.js from routes folder.
+
+app.use("/", require("./routes/index"));
+
 app.listen(8000, (err) => {
   if (err) {
     console.log(`Server not running, we got the error - ${err}`);
