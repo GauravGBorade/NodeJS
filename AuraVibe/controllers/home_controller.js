492a538256536873911a7b200b97fdd9d73e2663
Gaurav Borade <gauravborade618@gmail.com>
2023-07-14 22:56:03 +0530

    Code before adding async await.

--- AuraVibe/controllers/home_controller.js
+++ AuraVibe/controllers/home_controller.js
@@ -1,8 +1,7 @@
 const posts = require("../models/post");
 const user = require("../models/user");
+/* 
 module.exports.home = function (req, res) {
-  // console.log(req.cookies);
-  // res.cookie("user_id", 12);
   posts
     .find({})
     .populate("user")
@@ -25,4 +24,35 @@ module.exports.home = function (req, res) {
     .catch((err) => {
       console.log("error fetching posts", err);
     });
-};
+}; */
+
+//! converted above code to async await code
+
+try {
+  //* use try catch to catch errors.
+  module.exports.home = async function (req, res) {
+    //* first get all the posts with user and comments populated in it.
+    let posts = await posts
+      .find({})
+      .populate("user")
+      .populate({
+        path: "comments",
+        populate: {
+          path: "user",
+        },
+      });
+
+    //* then get all the users
+    let users = await user.find({});
+
+    //* then in the end send users and posts to browser.
+    return res.render("home", {
+      title: "AuraVibe Social",
+      posts: posts,
+      all_users: users,
+    });
+  };
+} catch (error) {
+  console.log("Error", error);
+  return;
+}
